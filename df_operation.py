fa335506d9961a1b8a67ef90b3247a577a503d55
Maxime Descourtieux <maximedescourt@hotmail.fr>
2023-09-27 12:34:49 +0200

    add docstring

--- df_operation.py
+++ df_operation.py
@@ -13,6 +13,20 @@ def add_statename_df_togbq(
     to_gbq: bool = False,
     table_id: str | None = None,
 ) -> pd.DataFrame:
+    """
+    Adds state names to a DataFrame by merging it with another DataFrame based on the 'state' column.
+
+    Args:
+        df_main (pandas.DataFrame): The main DataFrame to which state names will be added.
+        df_col (pandas.DataFrame): The DataFrame containing state names, with a column 'Abbreviation' for matching.
+        to_gbq (bool): If True, uploads the resulting DataFrame to Google BigQuery. Default is False.
+        table_id (str | None): The ID of the BigQuery table to which the DataFrame should be uploaded. Default is None.
+
+    Raises:
+        None
+
+    Returns:
+        pandas.DataFrame: The main DataFrame with state names added."""
     df = df_main.merge(df_col, left_on="state", right_on="Abbreviation")
     df.drop("state", axis=1, inplace=True)
     df.rename(columns={"State": "state", "Abbreviation": "abbreviation"}, inplace=True)
@@ -26,6 +40,19 @@ def state_abreviation_tobq(
     to_gbq: bool = False,
     table_id: str | None = None,
 ) -> pd.DataFrame:
+    """
+    Converts a DataFrame containing state abbreviations to a new DataFrame with unique values.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame containing state abbreviations.
+        to_gbq (bool): If True, uploads the resulting DataFrame to Google BigQuery. Default is False.
+        table_id (str | None): The ID of the BigQuery table to which the DataFrame should be uploaded. Default is None.
+
+    Raises:
+        None
+
+    Returns:
+        pandas.DataFrame: A new DataFrame with unique state abbreviations."""
     # Un peu inutile étant donné que j'ai déjà le state.csv mais ça me fait utiliser les fonctions
     df_state = df.drop(["gender", "year", "name", "number"], axis=1)
     df_state.drop_duplicates(inplace=True, ignore_index=True)
@@ -39,6 +66,19 @@ def name_occurence_df(
     to_gbq: bool = False,
     table_id: str | None = None,
 ) -> pd.DataFrame:
+    """
+    Generates a DataFrame that summarizes name occurrences.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame containing name data.
+        to_gbq (bool): If True, uploads the resulting DataFrame to Google BigQuery. Default is False.
+        table_id (str | None): The ID of the BigQuery table to which the DataFrame should be uploaded. Default is None.
+
+    Raises:
+        None
+
+    Returns:
+        pandas.DataFrame: A new DataFrame summarizing name occurrences."""
     df_name_oc = df.drop(["gender", "year", "state", "abbreviation"], axis=1)
     df_name_oc = df_name_oc.groupby("name").sum()
     # print(df_name_oc)
@@ -48,6 +88,17 @@ def name_occurence_df(
 
 
 def sort_by_names_occurence(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Sorts a DataFrame by the 'number' column in descending order.
+
+    Args:
+        df (pandas.DataFrame): The input DataFrame to be sorted.
+
+    Raises:
+        None
+
+    Returns:
+        pandas.DataFrame: The sorted DataFrame."""
     df.sort_values(by="number", ascending=False, inplace=True)
     print(df)
     return df
--- dl_up_gcp.py
+++ dl_up_gcp.py
@@ -8,6 +8,15 @@ import credentials as cred
 
 
 def init_blob_gcs():
+    """
+    Initializes and returns a Google Cloud Storage blob object.
+
+    Args:
+        None
+
+    Returns:
+        google.cloud.storage.blob.Blob: A blob object representing the specified file in Google Cloud Storage.
+    """
     storage_client = storage.Client(cred.PROJECT_ID)
     bucket = storage_client.get_bucket(cred.BUCKET_NAME)
     blob = bucket.blob(cred.FILE_NAME)
@@ -15,6 +24,17 @@ def init_blob_gcs():
 
 
 def dl_from_gcs_to_df_way1():
+    """
+    Downloads data from Google Cloud Storage and returns it as a DataFrame (way 1).
+
+    Args:
+        None
+
+    Raises:
+        None
+
+    Returns:
+        pandas.DataFrame: The data from Google Cloud Storage as a DataFrame."""
     blob = init_blob_gcs()
     # download as a string to put on dataframe : way 1
     contents = blob.download_as_string()
@@ -25,6 +45,17 @@ def dl_from_gcs_to_df_way1():
 
 
 def dl_from_gcs_to_df_way2():
+    """
+    Downloads data from Google Cloud Storage and returns it as a DataFrame (way 2).
+
+    Args:
+        None
+
+    Raises:
+        None
+
+    Returns:
+        pandas.DataFrame: The data from Google Cloud Storage as a DataFrame."""
     # download as a string to put on dataframe : way 2
     data_from_gcs_w2 = pd.read_csv(cred.GSUTIL_URI_FILE)
     print(data_from_gcs_w2)
@@ -32,17 +63,51 @@ def dl_from_gcs_to_df_way2():
 
 
 def dl_from_gcs_to_local():
+    """
+    Downloads a file from Google Cloud Storage to the local system.
+
+    Args:
+        None
+
+    Raises:
+        None
+
+    Returns:
+        None"""
     blob = init_blob_gcs()
     # to get the CSV file
     blob.download_to_filename("testname.csv")
 
 
 def df_from_local_csv(filename):
+    """
+    Reads a CSV file and returns its contents as a DataFrame.
+
+    Args:
+        filename (str): The path to the CSV file to be read.
+
+    Raises:
+        None
+
+    Returns:
+        pandas.DataFrame: The contents of the CSV file as a DataFrame."""
     df = pd.read_csv(filename)
     return df
 
 
 def df_to_bq(df: pd.DataFrame, tableId: str = cred.TABLE_ID_USANAMES):
+    """
+    Uploads a DataFrame to Google BigQuery.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame to be uploaded.
+        tableId (str): The ID of the BigQuery table to which the DataFrame should be uploaded. Default is cred.TABLE_ID_USANAMES.
+
+    Raises:
+        None
+
+    Returns:
+        None"""
     pgbq.to_gbq(df, tableId, project_id=cred.PROJECT_ID, if_exists="replace")
 
 

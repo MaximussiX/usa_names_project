7b49503dba3537e46a35921f3937f1286a106688
Maxime Descourtieux <maximedescourt@hotmail.fr>
2023-10-03 16:38:49 +0200

    nouvelle requete  + screenshot

--- SQL_BigQuery/view.sql
+++ SQL_BigQuery/view.sql
@@ -29,3 +29,41 @@ CREATE OR REPLACE VIEW `PROJECT_NAME.view_usanames.males_names`(name, number) AS
   ORDER BY
     nb DESC
 );
+
+
+# Top 3  female by year query :
+SELECT
+  finish_rank,
+  year,
+  gender,
+  name,
+  sum_by_year
+FROM (
+  SELECT
+    gender,
+    year,
+    name,
+    SUM(number) AS sum_by_year,
+    RANK() OVER (PARTITION BY year ORDER BY SUM(number) DESC) AS finish_rank
+  FROM
+    `PROJECT_NAME.dwh_usnames.usanames_raw`
+  WHERE
+    gender = 'F'
+  GROUP BY
+    gender,
+    year,
+    name
+  ORDER BY
+    year,
+    sum_by_year DESC)
+GROUP BY
+  finish_rank,
+  gender,
+  year,
+  name,
+  sum_by_year
+HAVING
+  finish_rank<4
+ORDER BY
+  year,
+  sum_by_year DESC
